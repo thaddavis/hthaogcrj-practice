499df458c059c8a75f816b322a6124750f7ef1e6
tad dy <a@a.com>
2024-10-31 02:05:20 +0000

    saving script for pt 7

--- helpers/send_email.py
+++ helpers/send_email.py
@@ -7,7 +7,7 @@ def send_email(recipients: list[str], body: str):
       "https://api.mailgun.net/v3/sandboxd7c358e02f26415dbb7329dd994a8334.mailgun.org/messages",
       auth=("api", os.getenv("MAILGUN_API_KEY")),
       data={
-        "from": "Wishbliss A.I. News Reporter <noreply@mail.wishbliss.link>",
+        "from": "A.I. News Reporter <noreply@mail.wishbliss.link>",
         "to": recipients,
         "subject": "Hello",
         "html": body,
